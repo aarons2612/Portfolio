0b74930218b4f4cdda4af0e43a441523e6168b1e
Aaron S <aaronsimon2612@gmail.com>
2024-07-27 21:13:11 +0530

    Added Header files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
 
 const Header = () => {
+  const handleResumeClick = () => {
+    const pdfUrl = '/Aaron\'s Resume.pdf'; // Path to your resume PDF
+    window.open(pdfUrl, '_blank'); // Open the PDF in a new tab
+  };
+
   return (
     <div className='header'>
-        <button className='res-btn'>Resume</button>
+      <button className='res-btn' onClick={handleResumeClick}>
+        Resume
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
